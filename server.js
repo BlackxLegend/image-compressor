c1d3ce3c05b1a36047ac4188364621abffaa194e
BlackxLegend <127228556+BlackxLegend@users.noreply.github.com>
2024-10-24 01:13:26 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the index.html on the root route
+// Serve index.html on root
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
